f0809bc328e920cd3f35fff6d7ecf90aadd6b5ee
uuso <mymail.uuso@gmail.com>
2019-12-10 10:54:28 +0300

    added model, template, view, url

--- project/newapp/models.py
+++ project/newapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Dummy(models.Model):
+    full_name = models.CharField(max_length=32, default="dummy")
+    def __str__(self):
+        return self.full_name
--- project/newapp/views.py
+++ project/newapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from models import Dummy
 
 # Create your views here.
+
+def index(request):
+    data = { "objects": Dummy.objects.all() }
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render(data))
