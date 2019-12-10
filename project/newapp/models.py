88d585c594b1d381aa227bb67e8c136ea027af0c
uuso <mymail.uuso@gmail.com>
2019-12-10 11:25:57 +0300

    add: field "age" to model "Dummy", migrations

--- project/newapp/models.py
+++ project/newapp/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Dummy(models.Model):
     full_name = models.CharField(max_length=32, default="dummy")
+    age = models.SmallIntegerField(default=25)
     def __str__(self):
-        return self.full_name
+        return "{}({})".format(self.full_name, self.age)
